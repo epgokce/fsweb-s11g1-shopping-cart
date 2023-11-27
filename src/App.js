3c56e9fc4dd795ccda858d793110a0a51a86b3ba
epgokce <ep.gokce@gmail.com>
2023-11-27 19:04:51 +0300

    Integrate ProductContext to manage products, cart, and addItem functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Route } from "react-router-dom";
 import { data } from "./data";
+import { ProductContext } from "./contexts/ProductContext";
 
 // Bileşenler
 import Navigation from "./components/Navigation";
@@ -17,18 +18,19 @@ function App() {
 
   return (
     <div className="App">
+
+<ProductContext.Provider value={ { products, cart, addItem } }>
       <Navigation cart={cart} />
 
       {/* Routelar */}
       <main className="content">
         <Route exact path="/">
-          <Products products={products} addItem={addItem} />
+          <Products/>
         </Route>
 
-        <Route path="/cart">
-          <ShoppingCart cart={cart} />
-        </Route>
+
       </main>
+      </ProductContext.Provider>
     </div>
   );
 }
